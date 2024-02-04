1cf646c420f84a150f5ab3cda1c26a1367eaff5c
Mou <madalmou.2004@gmail.com>
2024-02-04 23:26:03 +0530

    popup file is added

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+const content = ref(true);
+const popups = () => {
+  content.value = false;
+}
+</script>
+
+<template>
+  <div class="container">
+    <div v-if="content" class="popup">
+      <h2>Are You Sure?</h2>
+      <p>you want to delete this song</p>
+      <div class="bts">
+        <button @click="popups" id="btn">Yes</button>
+        <button id="btn_1">No</button>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
